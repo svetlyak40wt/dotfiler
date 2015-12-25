06457ae94bf9a4079552b4c40737453fb6228bfa
Alexander Lamaison <alexander.lamaison@gmail.com>
2015-12-25 21:47:02 +0000

    Windows support.

--- bin/lib/dot/real_filesystem.py
+++ bin/lib/dot/real_filesystem.py
@@ -20,9 +20,20 @@ class RealFS(object):
     def mkdir(self, path):
         os.mkdir(path)
 
-    def symlink(self, source, target):
+    def symlink(self, source, link_name):
         try:
-            os.symlink(source, target)
+            import os
+            os_symlink = getattr(os, "symlink", None)
+            if callable(os_symlink):
+                os_symlink(source, link_name)
+            else:
+                import ctypes
+                csl = ctypes.windll.kernel32.CreateSymbolicLinkW
+                csl.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
+                csl.restype = ctypes.c_ubyte
+                flags = 1 if os.path.isdir(source) else 0
+                if csl(link_name, source, flags) == 0:
+                    raise ctypes.WinError()
         except:
-            print 'CANT LINK: {1} -> {0}'.format(source, target)
+            print 'CANT LINK: {1} -> {0}'.format(source, link_name)
             raise
